74dd23ae730a1ad045d5a884bcc0fe71206490ff
PatStan <patrickstanton50@gmail.com>
2023-10-08 01:10:32 +0100

    Player and basic input

--- Script/GameManager.cs
+++ Script/GameManager.cs
@@ -1,16 +1,16 @@
 using Godot;
 using System;
 
-public partial class Icon : Sprite2D
+public partial class GameManager : Node
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Engine.MaxFps = 60;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		this.MoveLocalX(1);
 	}
 }
